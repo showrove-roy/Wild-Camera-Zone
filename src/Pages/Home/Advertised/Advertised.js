a98fd5af0b3caea51204768ed2209a35e5d5dd3e
Showrove Roy <showrove200@gmail.com>
2022-11-28 16:08:42 +0600

    ad product facing done

--- src/Pages/Home/Advertised/Advertised.js
+++ src/Pages/Home/Advertised/Advertised.js
@@ -1,7 +1,16 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import ProductCard from "../../../Components/ProductCard/ProductCard";
+import Loading from "../../Share/Loading/Loading";
 
 const Advertised = () => {
+  const { isLoading, data: products = [] } = useQuery({
+    queryKey: ["adproduct"],
+    queryFn: () =>
+      fetch("http://localhost:5000/product/ad").then((res) => res.json()),
+  });
+
+  if (isLoading) return <Loading></Loading>;
   return (
     <div className='lg:mx-5 mx-2 my-10'>
       <h3 className='capitalize mb-2 font-semibold text-2xl'>
@@ -10,7 +19,9 @@ const Advertised = () => {
       <div className='divider'></div>
       <div className=''>
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:gap-5 gap-y-5'>
-          {/* <ProductCard></ProductCard> */}
+          {products.map((product) => (
+            <ProductCard key={product._id} product={product}></ProductCard>
+          ))}
         </div>
       </div>
       <div className='divider'></div>
