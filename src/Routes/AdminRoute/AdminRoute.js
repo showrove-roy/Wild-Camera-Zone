01d9f4baa385d6e1ebb0731935d5df1881a0fd5a
Showrove Roy <showrove200@gmail.com>
2022-11-29 17:07:56 +0600

    Route loading problem solve

--- src/Routes/AdminRoute/AdminRoute.js
+++ src/Routes/AdminRoute/AdminRoute.js
@@ -2,11 +2,14 @@ import React from "react";
 import { Navigate, useLocation } from "react-router-dom";
 import { useAuth } from "../../Contexts/AuthProvider";
 import useUserType from "../../Hooks/useUserType/useUserType";
+import Loading from "../../Pages/Share/Loading/Loading";
 
 const AdminRoute = ({ children }) => {
   const { user } = useAuth();
   let location = useLocation();
-  const [userType] = useUserType(user.email);
+  const [userType, userLoading] = useUserType(user.email);
+
+  if (userLoading) return <Loading></Loading>;
 
   if (user?.uid && userType?.role === "admin") {
     return children;
